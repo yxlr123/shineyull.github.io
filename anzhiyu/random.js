0a33b73d6307b7edf33b8a3cd3113d2cf4fe75f9
ShineYu <shine.yulingle@gmail.com>
2023-01-04 03:15:10 +0800

    Site updated: 2023-01-04 03:15:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["EasyLua.html","EasyEngine.html","EasyDB.html","EasyDocker.html","EasyRPC.html","Design-Patterns.html","Game-making-from-my-perspective.html","GameUE-Slolo.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["EasyEngine.html","EasyLua.html","Design-Patterns.html","EasyDocker.html","Game-making-from-my-perspective.html","EasyDB.html","EasyRPC.html","GameUE-Slolo.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
